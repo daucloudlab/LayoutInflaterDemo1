0e041eb083fdf517f7a4545eba8bfd238fcfd5f9
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-20 12:10:59 +0600

    version 01

---
+++ app/src/main/java/kz/abcsoft/layoutinflaterdemo1/MainActivity.java
@@ -0,0 +1,55 @@
+package kz.abcsoft.layoutinflaterdemo1;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LayoutInflater ltInflater = getLayoutInflater() ;
+        View view = ltInflater.inflate(R.layout.test, null, false) ;
+        ViewGroup.LayoutParams lp = view.getLayoutParams();
+
+        Log.d(LOG_TAG, "Class of view: " + view.getClass().toString());
+        Log.d(LOG_TAG, "LayoutParams of view is null: " + (lp == null));
+        Log.d(LOG_TAG, "Text of view: " + ((TextView) view).getText());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
